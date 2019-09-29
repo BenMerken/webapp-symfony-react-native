95db74679b81ad2ec4eae712f4b5dca18127b942
Ben Merken <11701910@student.pxl.be>
2019-09-30 00:31:14 +0200

    Add PHPUnit unit tests for PDOAssetModel.php

--- src/Model/PDOAssetModel.php
+++ src/Model/PDOAssetModel.php
@@ -20,12 +20,13 @@ class PDOAssetModel implements AssetModel
     public function getIdForName($name)
     {
         $pdo = $this->connection->getPDO();
-        $query = "SELECT id from assets WHERE name = :name;";
+        $query = "SELECT id FROM assets WHERE name = :name;";
         $statement = $pdo->prepare($query);
         $statement->bindParam(":name", $name, PDO::PARAM_STR);
         $statement->bindColumn(1, $id, PDO::PARAM_INT);
-
-        if (!($id = $statement->execute())) {
+        $statement->execute();
+        $statement->fetch(PDO::FETCH_BOUND);
+        if (!$id) {
             throw new NotFoundHttpException("No asset found for name = $name.");
         }
 
--- tests/model/PDOAssetModelTest.php
+++ tests/model/PDOAssetModelTest.php
@@ -2,17 +2,67 @@
 
 namespace App\Tests\model;
 
+use App\Model\Connection;
+use App\Model\PDOAssetModel;
+use PDO;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PDOAssetModelTest extends TestCase
 {
+    private $connection;
+    private $pdo;
+
     public function setUp()
     {
-
+        $sql = file_get_contents(__DIR__ . "/../../asset-management-sqlite.sql");
+        $this->connection = new Connection("sqlite:memory:");
+        $this->pdo = $this->connection->getPDO();
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->exec($sql);
     }
 
     public function tearDown()
     {
+        $this->connection = null;
+        $this->pdo = null;
+    }
 
+    public function providerExistingNamesAndIds()
+    {
+        return [
+            [223, "beamer"],
+            [224, "computer"],
+            [225, "router"],
+        ];
+    }
+
+    public function providerNonExistingName()
+    {
+        return [
+            ["notInTheDB"]
+        ];
+    }
+
+    /**
+     * @dataProvider providerExistingNamesAndIds
+     */
+    public function testGetIdForName_existingNamesAndIds_true($expectedId, $name)
+    {
+        $assetModel = new PDOAssetModel($this->connection);
+        $actualId = $assetModel->getIdForName($name);
+        $this->assertEquals($expectedId, $actualId);
+    }
+
+    /**
+     * @dataProvider providerNonExistingName
+     */
+    public function testGetIdFromName_nonExistingName_throwsNotFoundHttpException($name)
+    {
+        $exceptionMessage = "No asset found for name = $name.";
+        $assetModel = new PDOAssetModel($this->connection);
+        $this->expectException(NotFoundHttpException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+        $assetModel->getIdForName($name);
     }
 }
