4bf4e2d0b5e6caf73bf38c0bddb4f84e1f6b7d83
Ben Merken <ben.merken@gmail.com>
2019-11-26 21:33:53 +0100

    Fix mistake in App.jsx
    - Fix mistake in App,jsx, where configureStore wasn't called but invoked
    - Remove development environment code from configureStore.ts

--- React Native/src/redux/configureStore.ts
+++ React Native/src/redux/configureStore.ts
@@ -1,15 +1,9 @@
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from "redux-thunk";
 
-const configureStore = (initialState) => {
-    const composeEnhancers =
-        window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose; // add support for Redux dev tools
-
-    return createStore(rootReducer, initialState,
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-    );
+const configureStore = () => {
+    return createStore(rootReducer, applyMiddleware(thunk));
 };
 
 export default configureStore;
