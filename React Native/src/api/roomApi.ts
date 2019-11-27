02085942af09e43175b1158174c8913788d0d653
Ben Merken <ben.merken@gmail.com>
2019-11-27 13:16:10 +0100

    Fix mistake in roomApi
    Make getRooms return the handling of the response to the API instead of just handling it but never returning it.

--- React Native/src/api/roomApi.ts
+++ React Native/src/api/roomApi.ts
@@ -2,8 +2,4 @@ import {handleResponse, handleError} from "./apiUtils";
 
 const baseUrl = 'http://192.168.33.22/rooms/';
 
-export const getRooms = () => {
-    fetch(baseUrl)
-        .then(handleResponse)
-        .catch(handleError);
-};
+export const getRooms = () => fetch(baseUrl).then(handleResponse).catch(handleError);
