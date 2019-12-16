c282638771e3f9260e161295e161a74379557be1
Ben Merken <ben.merken@gmail.com>
2019-12-16 17:52:37 +0100

    Add Asset endpoint to REST_API
    Add endpoint to get all Assets for certain room id

--- REST_API/src/Model/AssetModel.php
+++ REST_API/src/Model/AssetModel.php
@@ -6,6 +6,8 @@ namespace App\Model;
 
 interface AssetModel
 {
+    public function getAssetsForRoomId($roomId);
+
     public function getIdForName($name);
 
     public function validateName($name);
